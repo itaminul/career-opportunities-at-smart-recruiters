d28df3954649358c4880b6c94059479e3d80e84f
aminul@atilimited.net <aminul@atilimited.net>
2024-11-12 16:34:01 +0600

    create user table

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,15 +1,34 @@
-import { DataSource } from 'typeorm';
+import { DataSource, DataSourceOptions } from "typeorm";
+import { Users } from "./entity/Users";
 
-export const AppDataSource = new DataSource({
-  type: 'postgres',
-  host: 'localhost',
+// Export DataSourceOptions directly without instantiating DataSource
+export const AppDataSource: DataSourceOptions = {
+  type: "postgres",
+  host: "localhost",
   port: 5432,
-  username: 'erpdb',
-  password: '123456',
-  database: 'all_services_db',
-  synchronize: false,
+  username: "erpdb",
+  password: "123456",
+  database: "all_services_db",
+  entities: [Users],
+  migrations: ["./dist/migrations/*.js"],  // Path for TypeScript migrations
+  synchronize: true,
   logging: true,
-  entities: [],
-  migrations: ['./dist/migrations/*.js'],
-  subscribers: [],
-});
+};
+
+const dataSource = new DataSource(AppDataSource);
+export default dataSource;
+
+
+// npm run typeorm -- migration:generate -d ./dist/data-source.js ./src/migrations/CreateEmployeeAndDepartmentTables
+// npm run typeorm -- migration:run -d ./data-source.ts
+//npm run typeorm -- migration:run -d ./dist/data-source.js -- -n <MigrationName>
+
+// npm run migration:generate
+// npm run migration:run
+//npm run typeorm -- migration:run -d ./src/data-source.ts
+
+//npm run typeorm migration:create -n CreateUsersTable
+//npm run typeorm -- migration:run -d ./dist/data-source.js -- - n CreateUsersTable
+
+//npm run typeorm -- migration:run -d ./dist/data-source.js
+
