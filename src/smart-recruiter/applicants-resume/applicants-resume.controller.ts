8bb21b34c849bab71a5a29da0217021e9076218d
aminul@atilimited.net <aminul@atilimited.net>
2024-11-16 11:26:46 +0600

    upload file with save data

--- src/smart-recruiter/applicants-resume/applicants-resume.controller.ts
+++ src/smart-recruiter/applicants-resume/applicants-resume.controller.ts
@@ -1,13 +1,45 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import {
+  Body,
+  Controller,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from "@nestjs/common";
 import { ApplicantsResumeService } from "./applicants-resume.service";
 import { CreateResumeDto } from "./dto/create-resume.dto";
-import { Resume } from "@src/entity/resume";
-
+import { Resume } from "src/entity/resume";
+import { FileInterceptor } from "@nestjs/platform-express";
+import { diskStorage } from "multer";
+import { extname } from "path";
 @Controller("applicants-resume")
 export class ApplicantsResumeController {
   constructor(public readonly resumesService: ApplicantsResumeService) {}
+
+  @Post("upload")
+  @UseInterceptors(
+    FileInterceptor("file", {
+      storage: diskStorage({
+        destination: "./uploads/cv",
+        filename: (req, file, callback) => {
+          const name = file.originalname.split(".")[0];
+          const fileExtName = extname(file.originalname);
+          const randdomName = Array(4)
+            .fill(null)
+            .map(() => Math.random().toString(36).substring(2, 15))
+            .join("");
+          callback(null, `${name}-${randdomName}${fileExtName}`);
+        },
+      }),
+    })
+  )
+  async uploadResume(@UploadedFile() file: Express.Multer.File) {
+    const filePath = file.path;
+    const extractedData =
+      await this.resumesService.extractDataFromPDF(filePath);
+    return { extname, filePath };
+  }
   @Post()
-  async create(@Body() createResumeDto: CreateResumeDto): Promise<Resume> {
-    return this.resumesService.create(createResumeDto);
+  async saveResume(@Body() saveResume: CreateResumeDto): Promise<Resume> {
+    return this.resumesService.saveResume(saveResume);
   }
 }
--- src/smart-recruiter/applicants-resume/applicants-resume.module.ts
+++ src/smart-recruiter/applicants-resume/applicants-resume.module.ts
@@ -2,8 +2,8 @@ import { Module } from "@nestjs/common";
 import { ApplicantsResumeService } from "./applicants-resume.service";
 import { ApplicantsResumeController } from "./applicants-resume.controller";
 import { TypeOrmModule } from "@nestjs/typeorm";
-import { Resume } from "@src/entity/resume";
-import { Resume_attachments } from "@src/entity/resume_attachments";
+import { Resume } from "src/entity/resume";
+import { Resume_attachments } from "src/entity/resume_attachments";
 
 @Module({
   imports: [TypeOrmModule.forFeature([Resume, Resume_attachments])],
