61b6cdea00da948e9c8efdc6ff4c16b99fbece52
aminul@atilimited.net <aminul@atilimited.net>
2025-05-13 17:51:54 +0600

    login dto

---
+++ src/auth/dto/create-login.dto.ts
@@ -0,0 +1,74 @@
+import {
+  IsString,
+  IsEmail,
+  IsDateString,
+  IsOptional,
+  IsNotEmpty,
+} from "class-validator";
+
+class CreateLoginDto {
+  @IsOptional()
+  @IsString()
+  attachmentFile: string;
+  @IsOptional()
+  @IsString()
+  attachmentType: string;
+  @IsOptional()
+  @IsString()
+  attachmentPath: string;
+}
+
+export class CreateResumeDto {
+  @IsOptional()
+  @IsString()
+  name: string;
+
+  @IsNotEmpty()
+  @IsString()
+  username: string;
+
+  @IsNotEmpty()
+  @IsString()
+  password: string;
+
+  @IsOptional()
+  @IsString()
+  address: string;
+  @IsOptional()
+  @IsString()
+  phone: string;
+  @IsOptional()
+  @IsEmail()
+  email: string;
+  @IsOptional()
+  @IsString()
+  mobile: string;
+  @IsOptional()
+  @IsString()
+  experience: string;
+  @IsOptional()
+  @IsString()
+  present_salary: string;
+  @IsOptional()
+  @IsString()
+  expectedSalary: number;
+  @IsString()
+  noticePeriod: string;
+  @IsOptional()
+  currentSalary: number;
+  @IsOptional()
+  @IsString()
+  city: string;
+  @IsOptional()
+  @IsString()
+  district: string;
+  @IsOptional()
+  @IsString()
+  division: string;
+  @IsOptional()
+  @IsDateString()
+  dateOfBirth: string;
+  @IsOptional()
+  @IsString()
+  notice_period: string;
+}
